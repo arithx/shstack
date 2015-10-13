cff5ef472f0ed58da0148fb87ba869fe30c933ff
Stephen Lowrie <stephen.lowrie@gmail.com>
2015-10-13 12:42:10 -0500

    Added safety exits

--- tempest.sh
+++ tempest.sh
@@ -2,3 +2,4 @@ cd /opt/stack/new/tempest
 testr init
 testr run tempest.api.compute --subunit | subunit-trace --no-failure-debug -f
 subunit2html /opt/stack/new/tempest/.testrepository/0 /opt/stack/new/tempest/results.html
+exit
