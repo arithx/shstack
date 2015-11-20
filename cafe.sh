e9eedb38da3c74128a6feb5eeb79701414f418fc
Stephen Lowrie <stephen.lowrie@gmail.com>
2015-11-20 17:13:20 -0600

    Add temp file to tar

--- cafe.sh
+++ cafe.sh
@@ -34,5 +34,5 @@ echo $(nova image-list) >> /root/image_id
 
 
 cafe-runner compute cafe_devstack --result subunit --result-directory /root/
-
+tar -zcvf /root/cafe_logs.tar.gz /root/.opencafe/logs/compute/cafe_devstack.config/* /root/.opencafe/configs/compute/cafe_devstack.config /root/image_id
 tar -zcvf /root/cafe_logs.tar.gz /root/.opencafe/logs/compute/cafe_devstack.config/* /root/.opencafe/configs/compute/cafe_devstack.config
